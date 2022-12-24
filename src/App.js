e04352b7d30d7b1015003df6dcda90d2cbfbbc21
Jonathan Blandón <jblandon@unal.edu.co>
2022-12-23 21:31:21 -0500

    feat: modal agregado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Nav } from "./components/nv";
 import AppiCard from "./components/Cards";
+import "./sass/_gallery-modal.scss"
+import "./sass/_modal-cart.scss"
+import "./sass/_modal-navbar.scss"
+import "./sass/_variables.scss"
+import "./sass/_mixins.scss"
 import "./sass/cards.sass"
 import "./sass/nav.sass"
 import "./sass/popOverCarrito.scss"
